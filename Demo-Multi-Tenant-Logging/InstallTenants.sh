89c4f548b27e178aaca1c5f6b5feb5f7a0747cf2
priyavart kaushik <prikau@amazon.co.uk>
2021-05-22 11:47:17 +0000

    Add config files

---
+++ Demo-Multi-Tenant-Logging/InstallTenants.sh
@@ -0,0 +1,9 @@
+ClusterName='workshop-demo'
+LogRegion='eu-west-1'
+FluentBitHttpPort='2021'
+FluentBitReadFromHead='Off'
+tenant_name="$1"
+tenant_namespace="$1"
+[[ ${FluentBitReadFromHead} = 'On' ]] && FluentBitReadFromTail='Off'|| FluentBitReadFromTail='On'
+[[ -z ${FluentBitHttpPort} ]] && FluentBitHttpServer='Off' || FluentBitHttpServer='On'
+cat tenants.template.yaml | sed 's/{{cluster_name}}/'${ClusterName}'/;s/{{tenant_name}}/'${tenant_name}'/;s/{{tenant_namespace}}/'${tenant_namespace}'/;s/{{region_name}}/'${LogRegion}'/;s/{{http_server_toggle}}/"'${FluentBitHttpServer}'"/;s/{{http_server_port}}/"'${FluentBitHttpPort}'"/;s/{{read_from_head}}/"'${FluentBitReadFromHead}'"/;s/{{read_from_tail}}/"'${FluentBitReadFromTail}'"/' | kubectl apply -f - 
